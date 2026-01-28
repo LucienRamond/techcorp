03fe2162197bd1c1f7ba00b9dec0ec8aad7a3127
Lucien Ramond <lucien_ramond@hotmail.com>
2026-01-28 23:15:07 +0100

    Customize tool card

--- src/components/custom/ToolCard.tsx
+++ src/components/custom/ToolCard.tsx
@@ -9,10 +9,12 @@ import {
 } from "@/components/ui/card";
 import type { ToolsType } from "@/utils/types/tools";
 import {
+  EuroIcon,
   EyeIcon,
   MoreHorizontalIcon,
   PencilLineIcon,
   Trash2Icon,
+  UsersIcon,
 } from "lucide-react";
 import { Button } from "../ui/button";
 import {
@@ -23,10 +25,12 @@ import {
 } from "../ui/dropdown-menu";
 import Status from "./Status";
 import { displayIcon } from "./icon/DisplayIcon";
+import Icon from "./icon/Icon";
 
 export default function ToolCard({ tool }: { tool: ToolsType }) {
+  const last_update = new Date(tool.updated_at);
   return (
-    <Card>
+    <Card className=" bg-muted/15 shadow-xl hover:border-muted-foreground hover:bg-muted/50 duration-300 ease-in-out">
       <CardHeader>
         <CardTitle className=" flex gap-4 items-center">
           <div>{displayIcon(tool.icon_url, tool.owner_department)}</div>
@@ -37,7 +41,10 @@ export default function ToolCard({ tool }: { tool: ToolsType }) {
         <CardAction>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <Button variant="ghost" className="h-8 w-8 p-0">
+              <Button
+                variant="ghost"
+                className="h-8 w-8 p-0 bg-muted cursor-pointer"
+              >
                 <span className="sr-only">Open menu</span>
                 <MoreHorizontalIcon className="h-4 w-4" />
               </Button>
@@ -60,14 +67,50 @@ export default function ToolCard({ tool }: { tool: ToolsType }) {
         </CardAction>
       </CardHeader>
       <CardContent className=" grid grid-cols-2 gap-2 items-center">
-        <ul>
-          <li>{tool.monthly_cost}</li>
-          <li>{tool.owner_department}</li>
-          <li>{tool.active_users_count}</li>
+        <ul className="grid gap-1 font-bold text-2xl ">
+          <li className=" flex">
+            <EuroIcon
+              className="mr-2 bg-orange-gradient rounded p-1"
+              size={"1.8rem"}
+            />
+            <div>
+              €
+              {tool.monthly_cost
+                ? tool.monthly_cost.toLocaleString("en", {
+                    currency: "EUR",
+                  })
+                : 0}
+            </div>
+            <div className=" text-muted-foreground">/month</div>
+          </li>
+          <li className="flex gap-2 items-center">
+            <UsersIcon
+              className=" bg-blue-gradient rounded p-1"
+              size={"1.8rem"}
+            />
+            <div>{tool.active_users_count ? tool.active_users_count : 0}</div>
+            <div className=" text-muted-foreground">active users</div>
+          </li>
+          <li className=" flex gap-2">
+            <Icon department={tool.owner_department} />
+            <div>{tool.owner_department}</div>
+            <div className=" text-muted-foreground">department</div>
+          </li>
         </ul>
       </CardContent>
       <CardFooter>
-        <p>{tool.updated_at}</p>
+        <div className=" flex gap-2 italic">
+          <p className=" text-muted-foreground">Updated on </p>
+          <p>
+            {last_update &&
+              last_update.toLocaleDateString("en-US", {
+                month: "long",
+                day: "numeric",
+                year: "numeric",
+                hour: "2-digit",
+              })}
+          </p>
+        </div>
       </CardFooter>
     </Card>
   );
